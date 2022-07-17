ff6ec0ea9492e92ecb4bc636b800917ccfa45c01
Mykhailo Sichkaruk <mykhailo.sichkaruk@gmail.com>
2022-07-17 16:51:35 +0200

    Update data validation

--- src/server/routes/List.ts
+++ src/server/routes/List.ts
@@ -7,15 +7,32 @@ const prisma = new PrismaClient();
 
 export const List = Router();
 
+/**
+ * @api {post} /list Create a new list
+ * @apiName CreateList
+ * @apiGroup List
+ * @apiVersion 1.0.0
+ * @apiDescription Create a new list
+ * @apiHeader {String} Authorization Bearer + token
+ * @apiHeaderExample {json} Header-Example: Bearer + eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImNsNXBleWZjcDAwMDI1d3RhamFpYTFqY2MiLCJpYXQiOjE2NTgwNjgxMzIsImV4cCI6MTY1ODA3MTczMn0.5EPU0LzjBdK5gm3lp_f49C-yM5vu4eWHDALLXHCk7sg
+ * @apiParam {String} title Title of the list
+ * @apibody {String} body Body of the list 
+*/
 List.post("/",
-	body("title").isLength({ min: 1 }).withMessage("Title must be at least 1 characters long"),
+	body("title").exists().isLength({ min: 1 }).withMessage("Title must be at least 1 characters long"),
 
 	async (req, res) => {
-		const { name } = req.body;
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			res.status(400).json({ success: "false", message: "Invalid input", errors: errors.array() });
+			return;
+		}
+
+		const { title } = req.body;
 		const token = req.headers.authorization?.split(" ")[ 1 ] || "";
 
 		if (await verifyToken(token)) {
-			const list = await prisma.list.create({ data: { name, subscribers: { connect: { id: getTokenId(token) } } } });
+			const list = await prisma.list.create({ data: { title, subscribers: { connect: { id: getTokenId(token) } } } });
 			console.log("list: ", list);
 			res.status(200).json({ success: "true", message: list });
 		} else {
@@ -23,6 +40,10 @@ List.post("/",
 		}
 	});
 
+/**
+ * @api {delete} api/v1/list/ Subscribe to list
+ * @apiName DeleteList
+ */
 List.delete("/",
 	body("id").isLength({ min: 1 }).withMessage("Id must be at least 1 characters long"),
 	async (req, res) => {
@@ -42,24 +63,25 @@ List.delete("/",
 		}
 	});
 
-/** 
- * Update list name
- * @param {string} id
- * @param {string} title
+/**
+ * @api {put} api/v1/list/ Update all lists
+ * @apiName UpdateList
+ * @apiGroup List
+ * 
  */
 List.put("/",
-	body("id").isString().isLength({ min: 1 }).withMessage("Id must be at least 1 characters long"),
-	body("name").isString().isLength({ min: 1 }).withMessage("Name must be at least 1 characters long"),
+	body("id").exists().isString().isLength({ min: 1 }).withMessage("Id must be at least 1 characters long"),
+	body("name").exists().isString().isLength({ min: 1 }).withMessage("Name must be at least 1 characters long"),
 	async (req, res) => {
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
 			return res.status(400).json({ success: "false", messaga: "Incorrect input", errors: errors.array() });
 		}
 
-		const { id, name } = req.body;
+		const { id, title } = req.body;
 		const token = req.headers.authorization?.split(" ")[ 1 ] || "";
 		if (await verifyToken(token)) {
-			const list = await prisma.list.update({ where: { id }, data: { name } });
+			const list = await prisma.list.update({ where: { id }, data: { title } });
 			console.log("list: ", list);
 			res.status(200).json({ success: "true", message: list });
 		} else {
--- src/server/routes/Task.ts
+++ src/server/routes/Task.ts
@@ -1,7 +1,7 @@
-import { Router } from "express";
+import express, { Router } from "express";
 import { PrismaClient } from "@prisma/client";
 import { getTokenId, verifyToken  } from "./Auth";
-import { body } from "express-validator";
+import { body, Meta, validationResult } from "express-validator";
 
 const prisma = new PrismaClient();
 export const Task = Router();
@@ -10,25 +10,53 @@ Task.post("/",
 	body("title").isString().isLength({ min: 1 }).withMessage("Title must be at least 1 characters long"),
 	body("body").isString().isLength({ min: 1 }).withMessage("Body must be at least 1 characters long"),
 	body("listId").notEmpty().withMessage("ListId must be provided"),
-	body("status").isIn(["ACTIVE", "DONE", "CLOSED", "IN_PROGRESS"]).withMessage("Status must be : [ACTIVE, DONE, CLOSED, IN_PROGRESS]"),
-	body("deadline").isISO8601().withMessage("Deadline must be a valid date"),
-	async (req, res) => {
-		const { title, body, listId, status, deadline } = req.body;
-		if (listId === undefined) {
-			res.status(400).json({ success: "false", message: "List id is required" });
+	body("status").isIn(["ACTIVE", "DONE", "CLOSED", "IN_PROGRESS", undefined]).withMessage("Status must be in : [ACTIVE, DONE, CLOSED, IN_PROGRESS]"),
+	body("deadline").customSanitizer(checkDeadline).exists().withMessage("Deadline must be a valid date or empty"),
+	async (req: express.Request, res: express.Response) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			res.status(400).json({ success: "false", message: "Invalid input", errors: errors.array() });
 			return;
 		}
 
+		const { title, body, listId, status, deadline } = req.body;
 		const token = req.headers.authorization?.split(" ")[ 1 ] || "";
 		const authorId = getTokenId(token);
-		console.log("authorId: ", authorId);
+		const list = await prisma.list.findUnique({ where: { id: listId } });
 
-		if (await verifyToken(token)) {
-			const task = await prisma.task.create({ data: { title, body, listId, deadline, status, authorId } });
-			console.log("task: ", task);
-			res.status(200).json({ success: "true", message: task });
-		} else {
+		if (!verifyToken(token)) {
 			res.status(401).json({ success: "false", message: "Unauthorized" });
+			return;
 		}
+
+		if (!list) {
+			res.status(404).json({ success: "false", message: "List not found" });
+			return;
+		}
+
+		const user =  await prisma.list.findFirst({ where: { id: listId, subscribers: { some: { id: authorId } } } });
+
+		if (!user) {
+			res.status(404).json({ success: "false", message: `You are not member of ${list.title}. \nPlease ask author of this Todo-list to add you` });
+			return;
+		}
+
+		const task = await prisma.task.create({ data: { title, body, listId, deadline, status, authorId } });
+		await prisma.list.update({ where: { id: listId }, data: { items: { connect: { id: task.id } } } });
+		res.status(200).json({ success: "true", message: task });
 	});
 
+
+function checkDeadline(value: string | number | Date, { req }: Meta) {
+	if (!value) {
+		return new Date(Date.now() + (1000 * 60 * 60 * 24));
+	} else if (value instanceof Date) {
+		return req.body.deadline;
+	} else {
+		const date = new Date(value);
+		if (date.getTime() > Date.now()) {
+			return date;
+		}
+	}
+	return undefined;
+}
