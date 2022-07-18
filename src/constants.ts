a112f655251957913ce0eb142a3d5df297187f1e
Mykhailo Sichkaruk <mykhailo.sichkaruk@gmail.com>
2022-07-18 21:52:00 +0200

    Refactore constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,13 @@
 const AUTH = Object.freeze({
-    LOGIN_MIN_LENGTH: 3,
-    PASSWORD_MIN_LENGTH: 3,
-    TOKEN_VALIDATION_TIME: "1h",
+	PASSWORD: {
+		min: 3,
+		max: 255,
+	},
+	LOGIN: {
+		min: 3,
+		max: 255,
+	},
+	TOKEN_VALIDATION_TIME: "1h",
 });
 
 export { AUTH };
