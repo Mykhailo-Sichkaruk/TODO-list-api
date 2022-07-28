b8ed76ec616aa1f3b65917e8e870489b071944b3
Mykhailo Sichkaruk <mykhailo.sichkaruk@gmail.com>
2022-07-28 21:50:34 +0200

    Add test cases for List

--- src/test/List.ts
+++ src/test/List.ts
@@ -112,24 +112,189 @@ class List {
 			});
 	}
 
-	static getAll() {
-		it("200: Get list", List.#get200);
-		it("401: Unauthorized", Error.error401.bind(null, "/list", Method.GET));
-		it("444: JSON parse error", Error.error444.bind(null, "/list", Method.GET));
+	static async #notFound(method: Method, path: string): Promise<void> {
+		await supertest(app.app)[ method ](`${List.url}/${path}`)
+			.set("Authorization", `Bearer ${Auth.testUser[ 0 ].token}`)
+			.send({ id: "anyrandomid", title: "anyrandomtitle", userId: "not existing", listId: "not existing" })
+			.expect("Content-Type", /json/)
+			.expect(404)
+			.then(res => {
+				assert.equal(res.body.success, false);
+				assert.equal(res.body.message, "List not found");
+			});
 	}
 
-	static post() {
+	static async #delete406() {
+		await supertest(app.app)
+			.delete(`${List.url}`)
+			.set("Authorization", `Bearer ${Auth.testUser[ 1 ].token}`)
+			.send({ id: List.testList[ 0 ].id })
+			.expect("Content-Type", /json/)
+			.expect(406)
+			.then(res => {
+				assert.equal(res.body.success, false);
+				assert.ok(res.body.message.startsWith("You are not member of"));
+			});
+	}
+
+	static #getAll() {
+		it("200: Get list", List.#get200);
+		it("401: Unauthorized", Error.error401.bind(null, "/list", Method.GET));
+	}
+
+	static #post() {
 		it("200: Add item", List.#post200);
 		it("400: Invalid input", Error.error400.bind(null, "/list", Method.POST));
 		it("401: Unauthorized", Error.error401.bind(null, "/list", Method.POST));
 		it("444: JSON parse error", Error.error444.bind(null, "/list", Method.POST));
 	}
 
-	static delete() {
-		it("200: Delete item", Error.error200.bind(null, "/list", Method.DELETE));
+	static #delete() {
+		it("200: Delete item", List.#delete200);
 		it("401: Unauthorized", Error.error401.bind(null, "/list", Method.DELETE));
+		it("404: Not found", List.#notFound.bind(null, Method.DELETE, ""));
 		it("444: JSON parse error", Error.error444.bind(null, "/list", Method.DELETE));
+		it("406: You are not a member", List.#delete406);
 	}
+
+	static async #put200() {
+		const id = await List.#createList();
+		await supertest(app.app)
+			.put(`${List.url}`)
+			.set("Authorization", `Bearer ${Auth.testUser[ 0 ].token}`)
+			.send({ id, title: "New title" })
+			.expect("Content-Type", /json/)
+			.expect(200)
+			.then(res => {
+				assert.equal(res.body.success, true);
+				assert.equal(res.body.message.title, "New title");
+				assert.equal(res.body.message.id, id);
+			});
+
+		// Check if list is updated
+		await supertest(app.app)
+			.get(`${List.url}/${id}`)
+			.set("Authorization", `Bearer ${Auth.testUser[ 0 ].token}`)
+			.expect("Content-Type", /json/)
+			.expect(200)
+			.then(res => {
+				assert.equal(res.body.success, true);
+				assert.equal(res.body.message.title, "New title");
+				assert.equal(res.body.message.id, id);
+			});
+		// Delete list
+		await List.#deleteList(id);
+	}
+
+	static async #put406() {
+		const id = await List.#createList();
+		await supertest(app.app)
+			.put(`${List.url}`)
+			.set("Authorization", `Bearer ${Auth.testUser[ 1 ].token}`)
+			.send({ id, title: "New title" })
+			.expect("Content-Type", /json/)
+			.expect(406)
+			.then(res => {
+				assert.equal(res.body.success, false);
+				assert.ok(res.body.message.startsWith("You are not member of"));
+			});
+		// Delete list
+		await List.#deleteList(id);
+	}
+
+	static #put() {
+		it("200: Update item", List.#put200);
+		it("400: Invalid input", Error.error400.bind(null, "/list", Method.PUT));
+		it("401: Unauthorized", Error.error401.bind(null, "/list", Method.PUT));
+		it("404: Not found", List.#notFound.bind(null, Method.PUT, ""));
+		it("444: JSON parse error", Error.error444.bind(null, "/list", Method.PUT));
+		it("406: You are not a member", List.#put406);
+	}
+
+	static #getOne() {
+		it("200: Get one", List.#getOne200);
+		it("401: Unauthorized", Error.error401.bind(null, "/list/123456", Method.GET));
+		it("404: Not found", List.#notFound.bind(null, Method.GET, ""));
+	}
+
+	static test() {
+		describe("Get all", List.#getAll);
+		describe("Post", List.#post);
+		describe("Delete", List.#delete);
+		describe("Put", List.#put);
+		describe("Subscribe", List.#subscribe);
+		describe("Get One", List.#getOne);
+	}
+
+	static #subscribe() {
+		it("200: Subscribe", List.#subscribe200);
+		it("401: Unauthorized", Error.error401.bind(null, "/list/subscribe", Method.POST));
+		it("404: Not found", List.#notFound.bind(null, Method.POST, "subscribe"));
+		it("444: JSON parse error", Error.error444.bind(null, "/list/subscribe", Method.POST));
+	}
+
+	static async #subscribe200() {
+		const id = await List.#createList();
+		await supertest(app.app)
+			.post(`${List.url}/subscribe`)
+			.set("Authorization", `Bearer ${Auth.testUser[ 0 ].token}`)
+			.send({ userId: Auth.testUser[ 1 ].id, listId: id })
+			.expect("Content-Type", /json/)
+			.expect(200)
+			.then(res => {
+				assert.equal(res.body.success, true);
+				assert.equal(res.body.message, "Subscribed");
+			});
+		// Check if user is subscribed
+		await supertest(app.app)
+			.get(`${List.url}/`)
+			.set("Authorization", `Bearer ${Auth.testUser[ 1 ].token}`)
+			.expect("Content-Type", /json/)
+			.expect(200)
+			.then(res => {
+				assert.equal(res.body.success, true);
+				assert.equal(res.body.message[ 0 ].id, id);
+			});
+		// Delete list
+		await List.#deleteList(id);
+	}
+
+	static async #getOne200() {
+		const id = await List.#createList();
+		await supertest(app.app)
+			.get(`${List.url}/${id}`)
+			.set("Authorization", `Bearer ${Auth.testUser[ 1 ].token}`)
+			.expect("Content-Type", /json/)
+			.expect(200)
+			.then(res => {
+				assert.equal(res.body.success, true);
+				assert.equal(res.body.message.title, List.testList[ 0 ].title);
+				assert.equal(res.body.message.id, id);
+			});
+	}
+	// Delete list
+
+	// 	describe("get: GET", List.getAll);
+	// describe("post: POST", List.post);
+	// describe("put: PUT", () => {
+	// // it("Success 200: Update item", put200);
+	// 	it("Bad request 400", Error.error400.bind(null, "/list", Method.PUT));
+	// 	it("Unauthorized 401", Error.error401.bind(null, "/list", Method.PUT));
+	// 	// it("Not found 404", put404);
+	// 	// it("Not a member of list 403", put403);
+	// 	it("JSON parse error 444", Error.error444.bind(null, "/list", Method.PUT));
+	// });
+
+	// });
+	// describe("subscribe: POST", () => {
+	// // it("Success 200: Subscribe", subscribe200);
+	// 	it("Bad request 400", Error.error400.bind(null, "/list/subscribe", Method.POST));
+	// 	it("Unauthorized 401", Error.error401.bind(null, "/list/subscribe", Method.POST));
+	// 	// it("Not found 404", subscribe404);
+	// 	// it("Not a memver of list 403", subscribe403);
+	// 	it("JSON parse error 444", Error.error444.bind(null, "/list/subscribe", Method.POST));
+	// });
+
 
 }
 
--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,39 +1,10 @@
-import { Method } from "./../../typings/common";
 import { List } from "./List";
 import { Auth } from "./Auth";
-import Error from "./Error";
 
-describe("sequentially run tests", () => {
+describe("Test", () => {
 	describe("Autentication", () => {
-		describe("register: POST", Auth.testRegister);
+		describe("register", Auth.testRegister);
 		describe("login", Auth.testLogin);
 	});
-	describe("List", () => {
-		describe("get: GET", List.getAll);
-		describe("post: POST", List.post);
-		describe("put: PUT", () => {
-		// it("Success 200: Update item", put200);
-			it("Bad request 400", Error.error400.bind(null, "/list", Method.PUT));
-			it("Unauthorized 401", Error.error401.bind(null, "/list", Method.PUT));
-			// it("Not found 404", put404);
-			// it("Not a member of list 403", put403);
-			it("JSON parse error 444", Error.error444.bind(null, "/list", Method.PUT));
-		});
-		describe("delete: DELETE", () => {
-		// it("Success 200: Delete item", delete200);
-			it("Bad request 400", Error.error400.bind(null, "/list", Method.DELETE));
-			it("Unauthorized 401", Error.error401.bind(null, "/list", Method.DELETE));
-			// it("Not found 404", delete404);
-			// it("Not a member of list 403", delete403);
-			it("JSON parse error 444", Error.error444.bind(null, "/list", Method.DELETE));
-		});
-		describe("subscribe: POST", () => {
-		// it("Success 200: Subscribe", subscribe200);
-			it("Bad request 400", Error.error400.bind(null, "/list/subscribe", Method.POST));
-			it("Unauthorized 401", Error.error401.bind(null, "/list/subscribe", Method.POST));
-			// it("Not found 404", subscribe404);
-			// it("Not a memver of list 403", subscribe403);
-			it("JSON parse error 444", Error.error444.bind(null, "/list/subscribe", Method.POST));
-		});
-	});
+	describe("List", List.test);
 });
